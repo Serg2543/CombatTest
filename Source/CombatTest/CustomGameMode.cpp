c6c4f15183002fc057ce50bd80ea44d04078f0eb
Serg2543 <70772791+Serg2543@users.noreply.github.com>
2023-05-06 00:54:48 +0700

    Removed mannequin assets
    These are unnecessary now

--- Source/CombatTest/CustomGameMode.cpp
+++ Source/CombatTest/CustomGameMode.cpp
@@ -4,17 +4,10 @@
 #include "UObject/ConstructorHelpers.h"
 
 #include "CustomPlayerController.h"
-#include "CombatTestCharacter.h"
+//#include "CombatTestCharacter.h"
 
 ACustomGameMode::ACustomGameMode()
 {
 	// use our custom PlayerController class
 	PlayerControllerClass = ACustomPlayerController::StaticClass();
-
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDownCPP/Blueprints/TopDownCharacter"));
-	if (PlayerPawnBPClass.Class != nullptr)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
 }
